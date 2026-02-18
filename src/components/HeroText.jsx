88362e7ae300ee25fc7df49913c5b84bec6ba582
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-18 15:41:25 +0530

    added motion variants to mobile version

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,30 +1,109 @@
 import { FlipWords } from "./FlipWords";
+import { motion } from "motion/react";
 
 const HeroText = () => {
-    return (
-        <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
-            {/* desktop view */}
-            <div className="flex-col hidden md:flex c-space">
-                <h1 className="text-4xl font-medium">Hi I'm Sneha</h1>
-                <div className="flex flex-col items-start">
-                    <p className="text-5xl font-medium text-neutral-300"> 
-                        A Developer <br/> Dedicated to Crafting
-                    </p>
-                    {/* <get flip word from aceternity> */}
-                    <div>
-                        <FlipWords words = {["Secure", "Modern", "Scalable"]}
-                        duration={1800}
-                        className= "font-black text-white text-8xl"/>
-                    </div>
-                    <p className="text-4xl font-medium text-neutral-300">Web Solutions</p>
-                </div>
-            </div>
-                
-            {/* mobile view */}
+  const words = ["Secure", "Modern", "Scalable"];
+  const duration_of_flipWords = 1800;
+  const variants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 },
+  };
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+      {/* desktop view */}
+      <div className="flex-col hidden md:flex c-space">
+        <motion.h1
+          className="text-4xl font-medium"
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 1 }}
+        >
+          Hi I'm Sneha
+        </motion.h1>
+
+        <div className="flex flex-col items-start">
+          <motion.p
+            className="text-5xl font-medium text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.2 }}
+          >
+            A Developer <br /> Dedicated to Crafting
+          </motion.p>
+          {/* <get flip word from aceternity> */}
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.5 }}
+          >
+            <FlipWords
+              words={words}
+              duration={duration_of_flipWords}
+              className="font-black text-white text-8xl"
+            />
+          </motion.div>
+          <motion.p
+            className="text-4xl font-medium text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.2 }}
+          >
+            Web Solutions
+          </motion.p>
         </div>
-    );
+      </div>
+
+
+
+      {/* mobile view */}
+      <div className="flex flex-col space-y-6 md:hidden">
+        <motion.p 
+            className="text-4xl font-medium"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1 }}
+        >
+            Hi, I'm Sneha </motion.p>
+        <div>
+          <motion.p 
+            className="text-5xl font-black text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.2 }}
+          >
+            Building
+          </motion.p>
+          <motion.div
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.5 }}
+          >
+            <FlipWords
+              words={words}
+              duration={duration_of_flipWords}
+              className={"font-bold text-white text-7xl"}
+            />
+          </motion.div>
+          <motion.p 
+            className="text-4xl font-black text-neutral-300"
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1.8 }}
+          >
+            Web Applications
+          </motion.p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default HeroText
-
-
+export default HeroText;
