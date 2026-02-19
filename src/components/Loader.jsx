8e896e12f8d7a6d81e3918226474efabc1c5bca0
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-19 23:55:15 +0530

    added the loader text for the time when model is loading

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+// this lets us use html within canva
+import { Html, useProgress } from "@react-three/drei"
+
+// loader for displaying till the astronaut model loads
+const Loader = () => {
+    const { progress } = useProgress();
+    return (
+        <Html center className="text-xl font-black ">
+            {progress}%loaded</Html>
+            )  
+}
+
+export default Loader
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,13 @@
+// <Canvas> is the container where your 3D world lives.
 import { Canvas, useFrame } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Astronaut } from "../components/Astronaut";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
 
 
 
@@ -23,11 +27,19 @@ const Hero = () => {
       >
 
       <Canvas camera={{position : [0, 1, 3]}}>
-        <Astronaut 
-        // use these values if mobile version else use the default values in astronaut.jsx
-        scale = {isMobile && 0.23} 
-        position = {isMobile && [0, -1.5, 0]}
-        />
+        {/* till the model loads, to improve Ui, we will show a loader component */}
+        <Suspense fallback = {<Loader />}>
+
+        <Float>
+          <Astronaut 
+          // use these values if mobile version else use the default values in astronaut.jsx
+          scale = {isMobile && 0.23} 
+          position = {isMobile && [0, -1.5, 0]}
+          />
+        <Rig />
+        </Float>
+        
+          </Suspense>
         <OrbitControls />
       </Canvas>
       </figure>
@@ -35,4 +47,15 @@ const Hero = () => {
   );
 };
 
+// make the astronaut move as per our mouse position
+function Rig () {
+  return useFrame((state, delta) => {
+    easing.damp3(
+      state.camera.position,
+      [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+      0.5,
+      delta
+    )
+  })
+}
 export default Hero;
