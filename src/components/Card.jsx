ee82659bf455d226a627e6b5056ce069f740bfef
sneha31415 <sneha.singh.31415@gmail.com>
2026-02-21 15:10:41 +0530

    added drag of buttons via motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
+
 // grab cards
 const Card = ({ style, text, image }) => {
   return image && !text ? (
-    <img className='absolute w-15 cursor-grab' src  = {image} style={style}/>
+    <motion.img 
+    className="absolute w-15 cursor-grab" 
+    src={image} 
+    style={style}
+    whileHover = {{ scale : 1.05 }}
+    // to be able to drag the elements :
+    drag
+    />
   ) : (
-    <div 
-    className='absolute px-1 py-4 text-x; text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab'
-    style = {style}>
-        {text}
-    </div>
+    <motion.div
+      className="absolute px-1 py-4 text-x; text-center rounded-full ring ring-gray-700 font-extralight bg-storm w-[12rem] cursor-grab"
+      style={style}
+      whileHover = {{ scale : 1.05 }}
+      drag
+    >
+      {text}
+    </motion.div>
   );
 };
 
-export default Card
+export default Card;
